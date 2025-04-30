e3bef2256d87e29d89f90b3b536ba952509e93b7
MDorazh <Mykhailo.Dorazh@lnu.edu.ua>
2025-04-30 07:13:07 +0300

    Resolved merge conflict in main.cpp

--- Lab_0/main.cpp
+++ Lab_0/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
 int main() {
-<<<<<<< HEAD
-    std::cout << "Hello ASD!" << std::endl;
-=======
     std::cout << "Hello World!" << std::endl;
->>>>>>> 8db572f7d88b5e9b8c22304da5b62fa72dd23a5f
     return 0;
 }
